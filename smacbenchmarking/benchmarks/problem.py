61eeacfee9c9aa699e4e977ecc15bff2066069d3
benjamc <c.benjamins@ai.uni-hannover.de>
2023-07-06 14:27:48 +0200

    Change evaluate signature

--- smacbenchmarking/benchmarks/problem.py
+++ smacbenchmarking/benchmarks/problem.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 
 from ConfigSpace import ConfigurationSpace
-from smac.runhistory.dataclasses import TrialInfo
+from smac.runhistory.dataclasses import TrialInfo, TrialValue
 
 
 class Problem(ABC):
@@ -28,7 +28,7 @@ class Problem(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def evaluate(self, trial_info: TrialInfo) -> list[float] | float:
+    def evaluate(self, trial_info: TrialInfo) -> TrialValue:
         # TODO Maybe add logger to track all interesting info here
         """Evaluate problem.
 
@@ -39,43 +39,13 @@ class Problem(ABC):
 
         Returns
         -------
-        list[float] | float
-            Cost (vector).
-        """
-        raise NotImplementedError
-
-
-class SingleObjectiveProblem(Problem):
-    @abstractmethod
-    def evaluate(self, trial_info: TrialInfo) -> float:  # TODO Return runtime of one eval as well?
-        """Evaluate problem.
-
-        Parameters
-        ----------
-        trial_info : TrialInfo
-            Dataclass with configuration, seed, budget, instance.
-
-        Returns
-        -------
-        float
-            Cost
-        """
-        raise NotImplementedError
-
-
-class MultiObjectiveProblem(Problem):
-    @abstractmethod
-    def evaluate(self, trial_info: TrialInfo) -> list[float]:
-        """Evaluate problem.
-
-        Parameters
-        ----------
-        trial_info : TrialInfo
-            Dataclass with configuration, seed, budget, instance.
-
-        Returns
-        -------
-        list[float]
-            Cost vector.
+        TrialValue
+            Value of the trial, i.e.:
+                - cost : float | list[float]
+                - time : float, defaults to 0.0
+                - status : StatusType, defaults to StatusType.SUCCESS
+                - starttime : float, defaults to 0.0
+                - endtime : float, defaults to 0.0
+                - additional_info : dict[str, Any], defaults to {}
         """
         raise NotImplementedError
