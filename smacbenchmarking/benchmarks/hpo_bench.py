2dbe101dd7ccb97e4f4f0c2d5a72564e8dc8eaf8
Sarah Krebs <s.krebs@ai.uni-hannover.de>
2023-07-13 16:30:40 +0200

    Edit HPOBench benchmark

--- smacbenchmarking/benchmarks/hpo_bench.py
+++ smacbenchmarking/benchmarks/hpo_bench.py
@@ -1,39 +1,25 @@
 from __future__ import annotations
 
+from typing import Any
+
 import time
 
 from ConfigSpace import ConfigurationSpace
-
-from smacbenchmarking.benchmarks.problem import Problem
-from smacbenchmarking.utils.trials import TrialInfo, TrialValue
-
 from hpobench.container.benchmarks.ml.lr_benchmark import LRBenchmarkBB
 from hpobench.container.benchmarks.ml.nn_benchmark import NNBenchmarkBB
 from hpobench.container.benchmarks.ml.rf_benchmark import RandomForestBenchmarkBB
 from hpobench.container.benchmarks.ml.svm_benchmark import SVMBenchmarkBB
 from hpobench.container.benchmarks.ml.xgboost_benchmark import XGBoostBenchmarkBB
 
+from smacbenchmarking.benchmarks.problem import Problem
+from smacbenchmarking.utils.trials import TrialInfo, TrialValue
+
 
 class HPOBenchProblem(Problem):
     def __init__(self, model: str, task_id: int, seed: int):
         super().__init__()
-        self.model = model
-        self.task_id = task_id
 
-        if model == "lr":
-            self._problem = LRBenchmarkBB(rng=seed, task_id=task_id)
-        elif model == "nn":
-            self._problem = NNBenchmarkBB(rng=seed, task_id=task_id)
-        elif model == "rf":
-            self._problem = RandomForestBenchmarkBB(rng=seed, task_id=task_id)
-        elif model == "svm":
-            self._problem = SVMBenchmarkBB(rng=seed, task_id=task_id)
-        elif model == "xgboost":
-            self._problem = XGBoostBenchmarkBB(rng=seed, task_id=task_id, container_name="xgboost_benchmark",
-                                   container_source='./smacbenchmarking/singularity/containers/hpobench')
-        else:
-            raise ValueError(f"Unknown model {model} for HPOBench.")
-
+        self._problem = get_hpobench_problem(task_id=task_id, model=model, seed=seed)
         self._configspace = self._problem.get_configuration_space(seed=seed)
 
     @property
@@ -66,5 +52,61 @@ class HPOBenchProblem(Problem):
         result_dict = self._problem.objective_function(configuration=configuration, rng=seed)
         endtime = time.time()
         T = endtime - starttime
-        trial_value = TrialValue(cost=result_dict['function_value'], time=T, starttime=starttime, endtime=endtime)
+        trial_value = TrialValue(cost=result_dict["function_value"], time=T, starttime=starttime, endtime=endtime)
         return trial_value
+
+
+def get_hpobench_problem(model: str, task_id: int, seed: int) -> Any:
+    r"""Get HPOBench problem
+
+    Parameters
+    ----------
+    model : str
+    task_id : int
+    seed : int
+        Seed for configuration space.
+
+    Returns
+    -------
+    Any
+        Target function.
+    """
+    if model == "lr":
+        problem = LRBenchmarkBB(
+            rng=seed,
+            task_id=task_id,
+            container_name="lr_benchmark",
+            container_source="./smacbenchmarking/singularity/containers/hpobench",
+        )
+    elif model == "nn":
+        problem = NNBenchmarkBB(
+            rng=seed,
+            task_id=task_id,
+            container_name="nn_benchmark",
+            container_source="./smacbenchmarking/singularity/containers/hpobench",
+        )
+    elif model == "rf":
+        problem = RandomForestBenchmarkBB(
+            rng=seed,
+            task_id=task_id,
+            container_name="rf_benchmark",
+            container_source="./smacbenchmarking/singularity/containers/hpobench",
+        )
+    elif model == "svm":
+        problem = SVMBenchmarkBB(
+            rng=seed,
+            task_id=task_id,
+            container_name="svm_benchmark",
+            container_source="./smacbenchmarking/singularity/containers/hpobench",
+        )
+    elif model == "xgboost":
+        problem = XGBoostBenchmarkBB(
+            rng=seed,
+            task_id=task_id,
+            container_name="xgboost_benchmark",
+            container_source="./smacbenchmarking/singularity/containers/hpobench",
+        )
+    else:
+        raise ValueError(f"Unknown model {model} for HPOBench.")
+
+    return problem
