bf11a9f2b09db04509e84ea2e5cef2796460eb0c
Helena Graf <h.graf@ai.uni-hannover.de>
2023-08-03 13:59:28 +0200

    Update recipes

--- smacbenchmarking/container/container_app.py
+++ smacbenchmarking/container/container_app.py
@@ -16,23 +16,23 @@ if (job_id := os.environ['BENCHMARKING_JOB_ID']) != '':
     with open(f"{job_id}_config.txt", 'r') as f:
         hydra_config_path = f.read()
 
-    initialize(version_base=None, config_path="conf")
-    cfg = compose(hydra_config_path)
+initialize(version_base=None, config_path="conf")
+cfg = compose(hydra_config_path)
 
-    problem = make_problem(cfg=cfg)
+problem = make_problem(cfg=cfg)
 
-    app = Flask(__name__)
-
-
-    @app.route("/configspace", methods=["GET"])
-    def _request_configspace():
-        return json.dumps(cs_json.write(problem.configspace))
+app = Flask(__name__)
 
 
-    @app.route("/evaluate", methods=["POST"])
-    def _request_evaluation():
-        if request.is_json:
-            trial_info = TrialInfo(**json.loads(request.get_json()))
-            return json.dumps(problem.evaluate(trial_info).to_json())
-        else:
-            raise ValueError("Request is not JSON.")
+@app.route("/configspace", methods=["GET"])
+def _request_configspace():
+    return json.dumps(cs_json.write(problem.configspace))
+
+
+@app.route("/evaluate", methods=["POST"])
+def _request_evaluation():
+    if request.is_json:
+        trial_info = TrialInfo(**json.loads(request.get_json()))
+        return json.dumps(problem.evaluate(trial_info).to_json())
+    else:
+        raise ValueError("Request is not JSON.")
