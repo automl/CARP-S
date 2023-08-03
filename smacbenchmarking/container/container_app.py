da39bfaae3c6a3c44abfe8a66eec5a4c4a3f47e4
Helena Graf <h.graf@ai.uni-hannover.de>
2023-08-03 11:24:58 +0200

    Update recipes

--- smacbenchmarking/container/container_app.py
+++ smacbenchmarking/container/container_app.py
@@ -5,41 +5,34 @@ import os
 from ConfigSpace import ConfigurationSpace
 from ConfigSpace.read_and_write import json as cs_json
 from flask import Flask, request
+from hydra import initialize, compose
+from hydra.core.hydra_config import HydraConfig
 
+from smacbenchmarking.run import make_problem
 from smacbenchmarking.utils.trials import TrialInfo, TrialValue
 
-# command line arg
-print(sys.argv)
 
-if job_id := os.environ['SLURM_JOB_ID'] != '':
+if (job_id := os.environ['SLURM_JOB_ID']) != '':
     with open(f"{job_id}_config.txt", 'r') as f:
         hydra_config_path = f.read()
 
+    initialize(version_base=None, config_path="conf")
+    cfg = compose(hydra_config_path)
 
-# write argv to file
-with open('output.txt', 'w+') as f:
-    f.write(str(sys.argv) + '\n')
-    f.write(hydra_config_path)
+    problem = make_problem(cfg=cfg)
 
-configspace = ConfigurationSpace()
-
-app = Flask(__name__)
+    app = Flask(__name__)
 
 
-# TODO: remove (dummy app)
-def evaluate(trial_info: TrialInfo):
-    return TrialValue(cost=42)
+    @app.route("/configspace", methods=["GET"])
+    def _request_configspace():
+        return json.dumps(cs_json.write(problem.configspace))
 
 
-@app.route("/configspace", methods=["GET"])
-def _request_configspace():
-    return json.dumps(cs_json.write(configspace))
-
-
-@app.route("/evaluate", methods=["POST"])
-def _request_evaluation():
-    if request.is_json:
-        trial_info = TrialInfo(**json.loads(request.get_json()))
-        return json.dumps(evaluate(trial_info).to_json())
-    else:
-        raise ValueError("Request is not JSON.")
+    @app.route("/evaluate", methods=["POST"])
+    def _request_evaluation():
+        if request.is_json:
+            trial_info = TrialInfo(**json.loads(request.get_json()))
+            return json.dumps(problem.evaluate(trial_info).to_json())
+        else:
+            raise ValueError("Request is not JSON.")
