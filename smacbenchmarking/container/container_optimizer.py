51ea895ef8105235b438836dfd8ef57f18e3f5e6
Helena Graf <h.graf@ai.uni-hannover.de>
2023-08-03 15:12:56 +0200

    Update recipes

--- smacbenchmarking/container/container_optimizer.py
+++ smacbenchmarking/container/container_optimizer.py
@@ -10,8 +10,12 @@ if (job_id := os.environ['BENCHMARKING_JOB_ID']) != '':
     with open(f"{job_id}_config.txt", 'r') as f:
         hydra_config_path = f.read()
 
-    initialize(version_base=None, config_path="conf")
-    cfg = compose(hydra_config_path)
+    # path pattern runs/DUMMY_Optimizer/DUMMY/dummy/1/hydra_config.yaml
+    # we divide into hydra_config.yaml and rest of path
+    hydra_config_path = '../../' + hydra_config_path[:len(hydra_config_path) - 17]
+
+    initialize(version_base=None, config_path=hydra_config_path)
+    cfg = compose("hydra_config.yaml")
 
     problem = ContainerizedProblemClient()
     optimizer = make_optimizer(cfg=cfg, problem=problem)
