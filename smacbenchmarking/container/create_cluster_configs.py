8d9c73830c65444248a2fea387cd04ee6dad9edd
Helena Graf <h.graf@ai.uni-hannover.de>
2023-09-25 18:03:48 +0200

    Add creation of jobs for PyExperimenter

--- smacbenchmarking/container/create_cluster_configs.py
+++ smacbenchmarking/container/create_cluster_configs.py
@@ -1,6 +1,7 @@
 import hydra
 from domdf_python_tools.utils import printr
 from omegaconf import DictConfig, OmegaConf
+from py_experimenter.experimenter import PyExperimenter
 
 
 @hydra.main(config_path="../configs", config_name="base.yaml", version_base=None)  # type: ignore[misc]
@@ -18,13 +19,18 @@ def main(cfg: DictConfig) -> None:
     cfg_dict = OmegaConf.to_container(cfg=cfg, resolve=True)
     printr(cfg_dict)
 
-    # write hydra config to file
-    cfg_path = f"cluster_configs/{cfg.__hash__()}.yaml"
-    print(cfg_path)
-    OmegaConf.save(config=cfg, f=cfg_path)
+    experiment_configuration_file_path = 'smacbenchmarking/container/py_experimenter.cfg'
+    experimenter = PyExperimenter(experiment_configuration_file_path=experiment_configuration_file_path,
+                                  name='smacbenchmarking')
+
+    rows = [{
+        'config': cfg_dict,
+        'problem_id': cfg_dict["benchmark_id"],
+        'optimizer_id': cfg_dict["optimizer_id"],
+    }]
+    experimenter.fill_table_with_rows(rows)
     return None
 
 
 if __name__ == "__main__":
     main()
-
