cfcbb62ff160c856a860eab1adec5b953489e4c8
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2023-12-20 17:21:36 +0100

    Add more identifiers

--- smacbenchmarking/container/create_cluster_configs.py
+++ smacbenchmarking/container/create_cluster_configs.py
@@ -27,11 +27,16 @@ def main(cfg: DictConfig) -> None:
     rows = [
         {
             "config": cfg_dict,
-            "problem_id": cfg_dict["benchmark_id"],
+            "benchmark_id": cfg_dict["benchmark_id"],
+            "problem_id": cfg_dict["problem_id"],
             "optimizer_id": cfg_dict["optimizer_id"],
+            "seed": cfg_dict["seed"],
+            "task__n_trials": cfg_dict["task"]["n_trials"]
         }
     ]
     experimenter.fill_table_with_rows(rows)
+
+    # TODO: Return array info
     return None
 
 
