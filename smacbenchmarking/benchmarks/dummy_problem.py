e3bc8243e73c2788c7427f72d9a838729c4afc37
Helena Graf <h.graf@ai.uni-hannover.de>
2024-02-13 13:47:28 +0100

    Add hyperparameter to default problem

--- smacbenchmarking/benchmarks/dummy_problem.py
+++ smacbenchmarking/benchmarks/dummy_problem.py
@@ -1,13 +1,17 @@
-from ConfigSpace import ConfigurationSpace
+from ConfigSpace import ConfigurationSpace, UniformFloatHyperparameter
 
 from smacbenchmarking.benchmarks.problem import Problem
 from smacbenchmarking.utils.trials import TrialInfo, TrialValue
 
 
 class DummyProblem(Problem):
-    def __init__(self, return_value=0):
+    def __init__(self, return_value=0, lower=0, upper=1):
+        super().__init__()
         self._return_value = return_value
         self._configspace = ConfigurationSpace()
+        self._configspace.add_hyperparameter(
+            UniformFloatHyperparameter("x", lower=lower, upper=upper)
+        )
 
     @property
     def configspace(self) -> ConfigurationSpace:
