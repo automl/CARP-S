8b5964bdb2c020c995c270e9456d943fc107b9aa
benjamc <c.benjamins@ai.uni-hannover.de>
2024-02-21 13:42:47 +0100

    fix(build_script): fix path

--- scripts/build_containers.sh
+++ scripts/build_containers.sh
@@ -32,7 +32,7 @@ export CONT_OPT_RECIPE_PATH=container_recipes/optimizers
 # PROBLEMS
 # --------------------------------------------------------------------------------------------
 # Dummy Problem
-./compile_noctua2.sh ${CONT_BENCH_PATH}/DUMMY_Problem.sif ${CONT_BENCH_RECIPE_PATH}/container_recipes/DUMMY_Problem/DUMMY_Problem.recipe
+./compile_noctua2.sh ${CONT_BENCH_PATH}/DUMMY_Problem.sif ${CONT_BENCH_RECIPE_PATH}/DUMMY_Problem/DUMMY_Problem.recipe
 
 # HPOB
 ./compile_noctua2.sh ${CONT_BENCH_PATH}/HPOB.sif ${CONT_BENCH_RECIPE_PATH}/hpob/hpob_container.recipe
