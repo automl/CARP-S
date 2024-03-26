628e41ea2a8307e2519e773e45e8208a6fd7eca0
Sarah Krebs <s.krebs@ai.uni-hannover.de>
2024-03-26 16:21:27 +0100

    Use tabular hpobench benchmarks for blackbox setting

--- smacbenchmarking/benchmarks/hpo_bench.py
+++ smacbenchmarking/benchmarks/hpo_bench.py
@@ -1,3 +1,5 @@
+"""HPOBench problem class."""
+
 from __future__ import annotations
 
 from typing import Any, Optional
@@ -9,6 +11,7 @@ from hpobench.benchmarks.ml.lr_benchmark import LRBenchmark
 from hpobench.benchmarks.ml.nn_benchmark import NNBenchmark
 from hpobench.benchmarks.ml.rf_benchmark import RandomForestBenchmark
 from hpobench.benchmarks.ml.svm_benchmark import SVMBenchmark
+from hpobench.benchmarks.ml.tabular_benchmark import TabularBenchmark
 from hpobench.benchmarks.ml.xgboost_benchmark import XGBoostBenchmark
 
 from smacbenchmarking.benchmarks.problem import Problem
@@ -16,6 +19,8 @@ from smacbenchmarking.utils.trials import TrialInfo, TrialValue
 
 
 class HPOBenchProblem(Problem):
+    """HPOBench problem."""
+
     def __init__(self, model: str, task_id: int, seed: int, budget_type: Optional[str] = None):
         """Initialize a HPOBench problem.
 
@@ -24,12 +29,13 @@ class HPOBenchProblem(Problem):
         model : str Model name.
         task_id : str Task ID, see https://arxiv.org/pdf/2109.06716.pdf, page 22.
         seed: int Random seed.
-        budget_type : Optional[str] Budget type for the multifidelity setting. Should be None for the blackbox setting.
+        budget_type : Optional[str] Budget type for the multifidelity setting.
+                      Should be None for the blackbox setting.
         """
         super().__init__()
 
         self.budget_type = budget_type
-        self._problem = get_hpobench_problem(task_id=task_id, model=model, seed=seed)
+        self._problem = get_hpobench_problem(task_id=task_id, model=model, seed=seed, budget_type=self.budget_type)
         self._configspace = self._problem.get_configuration_space(seed=seed)
 
     @property
@@ -59,7 +65,7 @@ class HPOBenchProblem(Problem):
         configuration = trial_info.config
         starttime = time.time()
 
-        if self.budget_type is not None:
+        if trial_info.budget is not None:
             budget_value = float(trial_info.budget) if self.budget_type == "subsample" else round(trial_info.budget)
             fidelity = {self.budget_type: budget_value}
         else:
@@ -75,45 +81,36 @@ class HPOBenchProblem(Problem):
         return trial_value
 
 
-def get_hpobench_problem(model: str, task_id: int, seed: int) -> Any:
-    r"""Get HPOBench problem
+def get_hpobench_problem(model: str, task_id: int, seed: int, budget_type: Optional[str] = None) -> Any:
+    """Get HPOBench problem.
 
     Parameters
     ----------
-    model : str
-    task_id : int
-    seed : int
-        Seed for configuration space.
+        model : str Model name.
+        task_id : str Task ID, see https://arxiv.org/pdf/2109.06716.pdf, page 22.
+        seed: int Random seed.
+        budget_type : Optional[str] Budget type for the multifidelity setting.
+                      Should be None for the blackbox setting.
 
     Returns
     -------
     Any
         Target function.
     """
+    common_args = {"rng": seed, "task_id": task_id}
     if model == "lr":
-        problem = LRBenchmark(
-            rng=seed,
-            task_id=task_id,
-        )
+        problem = TabularBenchmark(model="lr", **common_args) if budget_type is None else LRBenchmark(**common_args)
     elif model == "nn":
-        problem = NNBenchmark(
-            rng=seed,
-            task_id=task_id,
-        )
+        problem = TabularBenchmark(model="nn", **common_args) if budget_type is None else NNBenchmark(**common_args)
     elif model == "rf":
-        problem = RandomForestBenchmark(
-            rng=seed,
-            task_id=task_id,
+        problem = (
+            TabularBenchmark(model="rf", **common_args) if budget_type is None else RandomForestBenchmark(**common_args)
         )
     elif model == "svm":
-        problem = SVMBenchmark(
-            rng=seed,
-            task_id=task_id,
-        )
+        problem = TabularBenchmark(model="svm", **common_args) if budget_type is None else SVMBenchmark(**common_args)
     elif model == "xgboost":
-        problem = XGBoostBenchmark(
-            rng=seed,
-            task_id=task_id,
+        problem = (
+            TabularBenchmark(model="xgb", **common_args) if budget_type is None else XGBoostBenchmark(**common_args)
         )
     else:
         raise ValueError(f"Unknown model {model} for HPOBench.")
