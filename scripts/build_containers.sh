74bf2f64396c749e226f7807452f2cf9927e7974
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2024-04-22 17:01:47 +0200

    fix: paths

--- scripts/build_containers.sh
+++ scripts/build_containers.sh
@@ -5,36 +5,40 @@ export CONT_BENCH_RECIPE_PATH=container_recipes/benchmarks
 export CONT_OPT_PATH=containers/optimizers
 export CONT_OPT_RECIPE_PATH=container_recipes/optimizers
 
+mkdir -p $CONT_GENERAL_PATH
+mkdir -p $CONT_BENCH_PATH
+mkdir -p $CONT_OPT_PATH
+
 # --------------------------------------------------------------------------------------------
 # HANDLERS (mandatory)
 # --------------------------------------------------------------------------------------------
-./compile_noctua2.sh ${CONT_GENERAL_PATH}/runner.sif ${CONT_GENERAL_RECIPE_PATH}/runner.recipe
+./scripts/compile_noctua2.sh ${CONT_GENERAL_PATH}/runner.sif ${CONT_GENERAL_RECIPE_PATH}/runner.recipe
 
 # --------------------------------------------------------------------------------------------
 # OPTIMIZERS
 # --------------------------------------------------------------------------------------------
 # Dummy Optimizer
-./compile_noctua2.sh ${CONT_OPT_PATH}/DUMMY_Optimizer.sif ${CONT_OPT_RECIPE_PATH}/DUMMY_Optimizer/DUMMY_Optimizer.recipe
+./scripts/compile_noctua2.sh ${CONT_OPT_PATH}/DUMMY_Optimizer.sif ${CONT_OPT_RECIPE_PATH}/DUMMY_Optimizer/DUMMY_Optimizer.recipe
 
 # Random Search
-./compile_noctua2.sh ${CONT_OPT_PATH}/RandomSearch.sif ${CONT_OPT_RECIPE_PATH}/randomsearch/randomsearch.recipe 
+./scripts/compile_noctua2.sh ${CONT_OPT_PATH}/RandomSearch.sif ${CONT_OPT_RECIPE_PATH}/RandomSearch/RandomSearch.recipe 
 
 # SMAC3-2.0
-./compile_noctua2.sh ${CONT_OPT_PATH}/SMAC3.sif ${CONT_OPT_RECIPE_PATH}/smac20/smac20.recipe 
+./scripts/compile_noctua2.sh ${CONT_OPT_PATH}/SMAC3.sif ${CONT_OPT_RECIPE_PATH}/SMAC3/SMAC3.recipe 
 
 # SMAC3-1.4
-./compile_noctua2.sh ${CONT_OPT_PATH}/SMAC3-1.4.sif ${CONT_OPT_RECIPE_PATH}/smac14/smac14.recipe 
+./scripts/compile_noctua2.sh ${CONT_OPT_PATH}/SMAC3-1.4.sif ${CONT_OPT_RECIPE_PATH}/SMAC3-1.4/SMAC3-1.4.recipe 
 
 # HEBO
-./compile_noctua2.sh ${CONT_OPT_PATH}/HEBO.sif ${CONT_OPT_RECIPE_PATH}/HEBO/HEBO.recipe 
+./scripts/compile_noctua2.sh ${CONT_OPT_PATH}/HEBO.sif ${CONT_OPT_RECIPE_PATH}/HEBO/HEBO.recipe 
 
 # --------------------------------------------------------------------------------------------
 # PROBLEMS
 # --------------------------------------------------------------------------------------------
 # Dummy Problem
-./compile_noctua2.sh ${CONT_BENCH_PATH}/DUMMY_Problem.sif ${CONT_BENCH_RECIPE_PATH}/DUMMY_Problem/DUMMY_Problem.recipe
+./scripts/compile_noctua2.sh ${CONT_BENCH_PATH}/DUMMY_Problem.sif ${CONT_BENCH_RECIPE_PATH}/DUMMY_Problem/DUMMY_Problem.recipe
 
 # HPOB
-./compile_noctua2.sh ${CONT_BENCH_PATH}/HPOB.sif ${CONT_BENCH_RECIPE_PATH}/hpob/hpob_container.recipe ${CONT_BENCH_RECIPE_PATH}/hpob/download_data.sh
+./scripts/compile_noctua2.sh ${CONT_BENCH_PATH}/HPOB.sif ${CONT_BENCH_RECIPE_PATH}/hpob/hpob_container.recipe ${CONT_BENCH_RECIPE_PATH}/hpob/download_data.sh
 
 
