49a652f5b72a77c9d4ace534b2620f84a323a903
Sohambasu07 <soham.basu07@gmail.com>
2024-05-03 17:31:38 +0200

    Added SKopt

---
+++ carps/optimizers/scikit_optimize.py
@@ -0,0 +1,183 @@
+"""Scikit-Optimize.
+
+* Scikit-Optimize optimizer for CARPS.
+* NOTE: Set to always minimize the cost
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+import ConfigSpace as CS
+import ConfigSpace.hyperparameters as CSH
+import numpy as np
+import skopt
+from skopt.space.space import Categorical, Integer, Real, Space
+
+from carps.optimizers.optimizer import Optimizer
+from carps.utils.trials import TrialInfo, TrialValue
+
+if TYPE_CHECKING:
+    from omegaconf import DictConfig
+
+    from carps.benchmarks.problem import Problem
+    from carps.loggers.abstract_logger import AbstractLogger
+    from carps.utils.types import Incumbent
+
+
+base_estimators = ["GP", "RF", "ET", "GBRT"]
+acq_funcs = ["LCB", "EI", "PI", "gp_hedge"]
+acq_optimizers = ["sampling", "lbfgs", "auto"]
+
+
+def CS_to_skopt_space(hp: CSH.Hyperparameter) -> Space:
+    """Convert ConfigSpace hyperparameter to skopt Space."""
+    if isinstance(hp, CSH.FloatHyperparameter):
+        if hp.log:
+            return Real(hp.lower, hp.upper, name=hp.name, prior="log-uniform")
+        else:
+            return Real(hp.lower, hp.upper, name=hp.name)
+    elif isinstance(hp, CSH.IntegerHyperparameter):
+        if hp.log:
+            return Integer(hp.lower, hp.upper, name=hp.name, prior="log-uniform")
+        else:
+            return Integer(hp.lower, hp.upper, name=hp.name)
+    elif isinstance(hp, CSH.CategoricalHyperparameter):
+        return Categorical(hp.choices, name=hp.name)
+    elif isinstance(hp, CSH.OrdinalHyperparameter):
+        return Categorical(hp.sequence, name=hp.name)
+    elif isinstance(hp, CSH.Constant):
+        return Categorical(list(hp.value), name=hp.name)
+    else:
+        raise NotImplementedError(
+            f"Unknown hyperparameter type: {hp.__class__.__name__}"
+        )
+
+
+class SkoptOptimizer(Optimizer):
+    """An optimizer that uses Scikit-Optimize to optimize a search space."""
+
+    def __init__(
+        self,
+        problem: Problem,
+        n_trials: int | None,
+        skopt_cfg: DictConfig,
+        optimizer_kwargs: DictConfig,
+        time_budget: float | None = None,
+        n_workers: int = 1,
+        loggers: list[AbstractLogger] | None = None,
+    ) -> None:
+        """Initialize a Scikit-Optimize optimizer."""
+        super().__init__(problem, n_trials, time_budget, n_workers, loggers)
+
+        self.fidelity_enabled = False
+        self.skopt_cfg = skopt_cfg
+        self.seed = skopt_cfg.seed
+        if self.seed is None:
+            self.seed = 0
+        np.random.seed(self.seed)
+
+        self.configspace = self.problem.configspace
+
+        self.skopt_space = self.convert_configspace(self.configspace)
+        self._solver: skopt.optimizer.Optimizer | None = None
+
+        self.optimizer_kwargs = optimizer_kwargs
+        assert self.optimizer_kwargs.base_estimator in base_estimators
+        assert self.optimizer_kwargs.acq_func in acq_funcs
+        assert self.optimizer_kwargs.acq_optimizer in acq_optimizers
+
+    def convert_configspace(self, configspace: CS.ConfigurationSpace) -> list[Space]:
+        """Convert ConfigSpace configuration space to Scikit-Optimize Space.
+
+        Parameters
+        ----------
+        configspace : ConfigurationSpace
+            Configuration space from Problem.
+
+        Returns
+        -------
+        list[Space]
+            Scikit-Optimize Space.
+        """
+        space = []
+        for hp in configspace.get_hyperparameters():
+            space.append(CS_to_skopt_space(hp))
+        return space
+
+    def convert_to_trial(  # type: ignore[override]
+        self, config: list[Any]
+    ) -> TrialInfo:
+        """Convert proposal by Scikit-Optimize to TrialInfo.
+
+        This ensures that the problem can be evaluated with a unified API.
+
+        Returns
+        -------
+        TrialInfo
+            Trial info containing configuration, budget, seed, instance.
+        """
+        configuration = CS.Configuration(
+            configuration_space=self.configspace,
+            values={
+                hp.name: value
+                for hp, value in zip(self.configspace.get_hyperparameters(), config)
+            },
+        )
+        trial_info = TrialInfo(
+            config=configuration, seed=self.seed, budget=None, instance=None
+        )
+        return trial_info
+
+    def _setup_optimizer(self) -> skopt.optimizer.Optimizer:
+        if self.optimizer_kwargs is None:
+            self.optimizer_kwargs = {}
+        opt = skopt.optimizer.Optimizer(
+            dimensions=self.skopt_space, **self.optimizer_kwargs
+        )
+        return opt
+
+    def ask(self) -> TrialInfo:
+        """Ask the optimizer for a new trial to evaluate.
+
+        If the optimizer does not support ask and tell,
+        raise `carps.utils.exceptions.AskAndTellNotSupportedError`
+        in child class.
+
+        Returns
+        -------
+        TrialInfo
+            trial info (config, seed, instance, budget)
+        """
+        config = self.solver.ask()
+        trial_info = self.convert_to_trial(config)
+        return trial_info
+
+    def tell(self, trial_info: TrialInfo, trial_value: TrialValue) -> None:
+        """Tell the optimizer a new trial.
+
+        If the optimizer does not support ask and tell,
+        raise `carps.utils.exceptions.AskAndTellNotSupportedError`
+        in child class.
+
+        Parameters
+        ----------
+        trial_info : TrialInfo
+            trial info (config, seed, instance, budget)
+        trial_value : TrialValue
+            trial value (cost, time, ...)
+        """
+        _ = self.solver.tell(list(trial_info.config.get_array()), trial_value.cost)
+
+    def get_current_incumbent(self) -> Incumbent:
+        """Extract the incumbent config and cost. May only be available after a complete run.
+
+        Returns
+        -------
+        Incumbent: tuple[TrialInfo, TrialValue] | list[tuple[TrialInfo, TrialValue]] | None
+            The incumbent configuration with associated cost.
+        """
+        best_result = self.solver.get_result()
+        best_config = self.convert_to_trial(best_result.x)
+        best_cost = TrialValue(cost=best_result.fun)
+        return (best_config, best_cost)
