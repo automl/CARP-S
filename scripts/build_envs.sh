b2d6a23d409c05f8062a99a2b4f1a1389c2efb3b
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2024-05-12 19:37:24 +0200

    build: more envs

--- scripts/build_envs.sh
+++ scripts/build_envs.sh
@@ -8,8 +8,8 @@ export CONT_OPT_PATH=containers/optimizers
 export CONT_OPT_RECIPE_PATH=container_recipes/optimizers
 export ENV_LOCATION="/scratch/hpc-prf-intexml/carps-neurips24/envs"
 
-bash ${CONT_BENCH_RECIPE_PATH}/HPOB/download_data.sh
-bash scripts/prepare_yahpo.sh
+# bash ${CONT_BENCH_RECIPE_PATH}/HPOB/download_data.sh
+# bash scripts/prepare_yahpo.sh
 
 bash scripts/build_env.sh HPOB SMAC3 3.11 $ENV_LOCATION
 bash scripts/build_env.sh HPOB SMAC3-1.4 3.11 $ENV_LOCATION
@@ -21,18 +21,23 @@ bash scripts/build_env.sh YAHPO SMAC3-1.4 3.11 $ENV_LOCATION
 bash scripts/build_env.sh YAHPO RandomSearch 3.11 $ENV_LOCATION
 bash scripts/build_env.sh YAHPO HEBO 3.11 $ENV_LOCATION
 bash scripts/build_env.sh YAHPO SyneTune 3.11 $ENV_LOCATION
-bash scripts/build_env.sh HPOBench SMAC3 3.10 $ENV_LOCATION
-bash scripts/build_env.sh HPOBench SMAC3-1.4 3.10 $ENV_LOCATION
-bash scripts/build_env.sh HPOBench RandomSearch 3.10 $ENV_LOCATION
-bash scripts/build_env.sh HPOBench HEBO 3.10 $ENV_LOCATION
-bash scripts/build_env.sh HPOBench SyneTune 3.10 $ENV_LOCATION
+bash scripts/build_env.sh HPOBench SMAC3 3.11 $ENV_LOCATION
+bash scripts/build_env.sh HPOBench SMAC3-1.4 3.11 $ENV_LOCATION
+bash scripts/build_env.sh HPOBench RandomSearch 3.11 $ENV_LOCATION
+bash scripts/build_env.sh HPOBench HEBO 3.11 $ENV_LOCATION
+bash scripts/build_env.sh HPOBench SyneTune 3.11 $ENV_LOCATION
 bash scripts/build_env.sh BBOB SMAC3 3.11 $ENV_LOCATION
 bash scripts/build_env.sh BBOB SMAC3-1.4 3.11 $ENV_LOCATION
 bash scripts/build_env.sh BBOB RandomSearch 3.11 $ENV_LOCATION
 bash scripts/build_env.sh BBOB HEBO 3.11 $ENV_LOCATION
 bash scripts/build_env.sh BBOB SyneTune 3.11 $ENV_LOCATION
+bash scripts/build_env.sh MFPBench SMAC3 3.11 $ENV_LOCATION
+bash scripts/build_env.sh MFPBench SMAC3-1.4 3.11 $ENV_LOCATION
+bash scripts/build_env.sh MFPBench RandomSearch 3.11 $ENV_LOCATION
+bash scripts/build_env.sh MFPBench HEBO 3.11 $ENV_LOCATION
+bash scripts/build_env.sh MFPBench SyneTune 3.11 $ENV_LOCATION
 
-# for benchmark in "HPOB" "YAHPO" "HPOBench" "BBOB" 
+# for benchmark in "HPOB" "YAHPO" "HPOBench" "BBOB" "MFPBench"
 # do
 #     for optimizer in "SMAC3" "SMAC3-1.4" "RandomSearch" "HEBO" "SyneTune"
 #     do
