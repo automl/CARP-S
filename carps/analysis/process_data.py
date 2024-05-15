cec8fd65fda15b968a8aeb746b68b6df07094f16
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2024-05-15 14:14:45 +0200

    feat(analysis): add task if missing in logs and scenario type

--- carps/analysis/process_data.py
+++ carps/analysis/process_data.py
@@ -5,11 +5,62 @@ from pathlib import Path
 import fire
 import numpy as np
 import pandas as pd
+from omegaconf import DictConfig, OmegaConf, ListConfig
 
+from carps.analysis.gather_data import annotate_with_cfg
 from carps.utils.loggingutils import get_logger, setup_logging
+from carps.utils.task import Task
 
 setup_logging()
 logger = get_logger(__file__)
+
+def add_scenario_type(logs: pd.DataFrame) -> pd.DataFrame:
+    def determine_scenario_type(x: pd.Series) -> str:
+        if x["task.is_multifidelity"] is False and x["task.is_multiobjective"] is False:
+            scenario = "blackbox"
+        elif x["task.is_multifidelity"] is True and x["task.is_multiobjective"] is False:
+            scenario = "multi-fidelity"
+        elif x["task.is_multifidelity"] is False and x["task.is_multiobjective"] is True:
+            scenario = "multi-objective"
+        elif x["task.is_multifidelity"] is True and x["task.is_multiobjective"] is True:
+            scenario = "multi-fidelity-objective"
+        elif np.isnan(x["task.is_multifidelity"]) or np.isnan(x["task.is_multiobjective"]):
+            scenario = "blackbox"
+        else:
+            print(x["problem_id"], x["optimizer_id"], x["seed"], x["task.is_multifidelity"], type(x["task.is_multifidelity"]))
+            raise ValueError("Unknown scenario")
+        return scenario
+    logs["scenario"] = logs.apply(determine_scenario_type, axis=1)
+    return logs
+
+def maybe_postadd_task(logs: pd.DataFrame) -> pd.DataFrame:
+    index_fn = Path(__file__).parent.parent / "configs/problem/index.csv"
+    if not index_fn.is_file():
+        raise ValueError("Problem ids have not been indexed. Run `python -m carps.utils.index_configs`.")
+    problem_index = pd.read_csv(index_fn)
+
+    def load_task_cfg(problem_id: str) -> DictConfig:
+        config_fn = problem_index["config_fn"][problem_index["problem_id"]==problem_id].iloc[0]
+        cfg = OmegaConf.load(config_fn)
+        task_cfg = cfg.task
+        return task_cfg
+    
+    new_logs = []    
+    for gid, gdf in logs.groupby(by="problem_id"):
+        task_cfg = load_task_cfg(problem_id=gid)
+        task_columns = [c for c in gdf.columns if c.startswith("task.")]
+        for c in task_columns:
+            key = c.split(".")[1]
+            if np.nan in gdf[c].unique():
+                print(c, key, task_cfg, gid)
+                v = task_cfg.get(key)
+                if isinstance(v, (list, ListConfig)):
+                    v = [v] * len(gdf)
+                gdf[c] = v
+        new_logs.append(gdf)    
+    logs = pd.concat(new_logs)
+    return logs
+
 
 def process_logs(logs: pd.DataFrame) -> pd.DataFrame:
     logger.info("Processing raw logs. Normalize n_trials and costs. Calculate trajectory (incumbent cost).")
@@ -25,6 +76,10 @@ def process_logs(logs: pd.DataFrame) -> pd.DataFrame:
     logs["trial_value__cost_norm"] = logs.groupby("problem_id")["trial_value__cost"].transform(normalize)
     logs["trial_value__cost_inc"] = logs.groupby(by=["problem_id", "optimizer_id", "seed"])["trial_value__cost"].transform("cummin")
     logs["trial_value__cost_inc_norm"] = logs.groupby(by=["problem_id", "optimizer_id", "seed"])["trial_value__cost_norm"].transform("cummin")
+    logs = maybe_postadd_task(logs)
+    if "task.n_objectives" in logs:
+        logs["task.is_multiobjective"] = logs["task.n_objectives"] > 1
+    logs = add_scenario_type(logs)
 
     # Add time
     logs = logs.groupby(by=["problem_id", "optimizer_id", "seed"]).apply(calc_time).reset_index(drop=True)
@@ -77,7 +132,7 @@ def get_interpolated_performance_df(logs: pd.DataFrame, n_points: int = 20, x_co
     interpolation_columns = ["trial_value__cost", "trial_value__cost_norm", "trial_value__cost_inc", "trial_value__cost_inc_norm"]
     # interpolation_columns = [
     #     c for c in logs.columns if c != x_column and c not in identifier_columns and not c.startswith("problem")]
-    group_keys = ["benchmark_id", "optimizer_id", "problem_id", "seed"]
+    group_keys = ["scenario", "benchmark_id", "optimizer_id", "problem_id", "seed"]
     x = np.linspace(0, 1, n_points + 1)
     D = []
     for gid, gdf in logs.groupby(by=group_keys):
