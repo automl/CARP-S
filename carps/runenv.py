9a62dacbe7cc8427e80463f5a37b8c6567ec8b26
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2024-05-24 13:55:09 +0200

    feat: draft to activate env before running

---
+++ carps/runenv.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+import subprocess
+from pathlib import Path
+
+import hydra
+from hydra.core.hydra_config import HydraConfig
+from omegaconf import DictConfig
+
+from carps.utils.loggingutils import get_logger, setup_logging
+
+setup_logging()
+logger = get_logger(__file__)
+
+
+@hydra.main(config_path="configs", config_name="base.yaml", version_base=None)  # type: ignore[misc]
+def main(cfg: DictConfig) -> None:
+    """Run optimizer on problem.
+
+    Save trajectory and metadata to database.
+
+    Parameters
+    ----------
+    cfg : DictConfig
+        Global configuration.
+
+    """
+    hydra_cfg = HydraConfig.instance().get()
+    overrides = hydra_cfg.overrides.task
+    runcommand = f"python -m carps.run {' '.join(overrides)}"
+    logger.info(f"Runcommand: `{runcommand}`")
+    env_location = Path(cfg.conda_env_location) / cfg.conda_env_name
+    logger.info(f"Selected environment: {env_location}")
+    command = f"source ~/.bashrc; micromamba run -p {env_location} {runcommand}"
+    logger.info(command)
+    subprocess.run(command, shell=True)
+
+    return None
+
+
+if __name__ == "__main__":
+    main()
