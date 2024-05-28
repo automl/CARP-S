cdd533cb59f00b6cc729aedffb9da54f99598f76
timruhkopf <timruhkopf@gmail.com>
2024-05-28 16:50:08 +0200

    [feature] MOASHA

--- carps/optimizers/synetune.py
+++ carps/optimizers/synetune.py
@@ -5,21 +5,41 @@ import datetime
 from collections import OrderedDict
 from typing import Any, Callable
 
+import numpy as np
 from ConfigSpace import Configuration, ConfigurationSpace
-from ConfigSpace.hyperparameters import (CategoricalHyperparameter, Constant,
-                                         FloatHyperparameter, Hyperparameter,
-                                         IntegerHyperparameter,
-                                         OrdinalHyperparameter)
+from ConfigSpace.hyperparameters import (
+    CategoricalHyperparameter,
+    Constant,
+    FloatHyperparameter,
+    Hyperparameter,
+    IntegerHyperparameter,
+    OrdinalHyperparameter,
+)
+from pymoo.util.nds.non_dominated_sorting import NonDominatedSorting
 from syne_tune.backend.trial_status import Status
 from syne_tune.backend.trial_status import Trial as SyneTrial
 from syne_tune.backend.trial_status import TrialResult
-from syne_tune.config_space import (choice, lograndint, loguniform, ordinal,
-                                    randint, uniform)
-from syne_tune.optimizer.baselines import (ASHA, BOHB, BORE, DEHB, KDE,
-                                           MOBSTER, MOREA,
-                                           BayesianOptimization,
-                                           MOLinearScalarizationBayesOpt,
-                                           MORandomScalarizationBayesOpt)
+from syne_tune.config_space import (
+    choice,
+    lograndint,
+    loguniform,
+    ordinal,
+    randint,
+    uniform,
+)
+from syne_tune.optimizer.baselines import (
+    ASHA,
+    BOHB,
+    BORE,
+    DEHB,
+    KDE,
+    MOASHA,
+    MOBSTER,
+    MOREA,
+    BayesianOptimization,
+    MOLinearScalarizationBayesOpt,
+    MORandomScalarizationBayesOpt,
+)
 from syne_tune.optimizer.scheduler import TrialScheduler as SyneTrialScheduler
 
 from carps.benchmarks.problem import Problem
@@ -41,9 +61,13 @@ optimizers_dict = {
     "KDE": KDE,
     "BORE": BORE,
     "DEHB": DEHB,
+    "MOASHA": MOASHA,
 }
 
-mf_optimizer_dicts = {"with_mf": {"ASHA", "DEHB", "MOBSTER", "BOHB"}, "without_mf": {"BORE", "BayesianOptimization", "KDE"}}
+mf_optimizer_dicts = {
+    "with_mf": {"ASHA", "MOASHA", "DEHB", "MOBSTER", "BOHB"},
+    "without_mf": {"BORE", "BayesianOptimization", "KDE"},
+}
 
 
 def configspaceHP2syneTuneHP(hp: Hyperparameter) -> Callable:
@@ -64,7 +88,9 @@ def configspaceHP2syneTuneHP(hp: Hyperparameter) -> Callable:
     elif isinstance(hp, Constant):
         return choice([hp.value])
     else:
-        raise NotImplementedError(f"Unknown hyperparameter type: {hp.__class__.__name__}")
+        raise NotImplementedError(
+            f"Unknown hyperparameter type: {hp.__class__.__name__}"
+        )
 
 
 class SynetuneOptimizer(Optimizer):
@@ -84,10 +110,14 @@ class SynetuneOptimizer(Optimizer):
             # raise NotImplementedError("Multi-Fidelity Optimization on SyneTune is not implemented yet!")
             self.fidelity_enabled = True
             if self.task.fidelity_type is None:
-                raise ValueError("To run multi-fidelity optimizer, the problem must define a fidelity type!")
+                raise ValueError(
+                    "To run multi-fidelity optimizer, the problem must define a fidelity type!"
+                )
             if self.max_budget is None:
-                raise ValueError("To run multi-fidelity optimizer, we must specify max_budget!")
-        
+                raise ValueError(
+                    "To run multi-fidelity optimizer, we must specify max_budget!"
+                )
+
         self.fidelity_type: str = self.task.fidelity_type
         self.configspace = self.problem.configspace
         self.syne_tune_configspace = self.convert_configspace(self.configspace)
@@ -95,11 +125,13 @@ class SynetuneOptimizer(Optimizer):
         self.trial_counter: int = 0
 
         self.optimizer_name = optimizer_name
-        self._solver: SyneTrialScheduler | None = None 
+        self._solver: SyneTrialScheduler | None = None
 
         self.optimizer_kwargs = optimizer_kwargs
 
-        self.completed_experiments: OrderedDict[int, tuple[TrialValue, TrialInfo]] = OrderedDict()
+        self.completed_experiments: OrderedDict[
+            int, tuple[TrialValue, TrialInfo]
+        ] = OrderedDict()
 
     def convert_configspace(self, configspace: ConfigurationSpace) -> dict[str, Any]:
         """Convert configuration space from Problem to Optimizer.
@@ -124,9 +156,7 @@ class SynetuneOptimizer(Optimizer):
             configspace_st[self.fidelity_type] = self.max_budget
         return configspace_st
 
-    def convert_to_trial(  # type: ignore[override]
-        self, trial: SyneTrial
-    ) -> TrialInfo:
+    def convert_to_trial(self, trial: SyneTrial) -> TrialInfo:  # type: ignore[override]
         """Convert proposal from SyneTune to TrialInfo.
 
         Parameters
@@ -150,8 +180,12 @@ class SynetuneOptimizer(Optimizer):
             budget = configs.pop(self.fidelity_type)
         else:
             budget = None
-        configuration = Configuration(configuration_space=self.configspace, values=configs)
-        trial_info = TrialInfo(config=configuration, seed=None, budget=budget, instance=None)
+        configuration = Configuration(
+            configuration_space=self.configspace, values=configs
+        )
+        trial_info = TrialInfo(
+            config=configuration, seed=None, budget=budget, instance=None
+        )
         return trial_info
 
     def ask(self) -> TrialInfo:
@@ -174,7 +208,7 @@ class SynetuneOptimizer(Optimizer):
         )
         trial_info = self.convert_to_trial(trial=trial)
         return trial_info
-    
+
     def convert_to_synetrial(self, trial_info: TrialInfo) -> SyneTrial:
         """Convert a trial info to the syne tune format.
 
@@ -215,7 +249,16 @@ class SynetuneOptimizer(Optimizer):
         cost = trial_value.cost
         trial = self.convert_to_synetrial(trial_info=trial_info)
         experiment_result = {self.metric: cost}
+        if self.optimizer_name == "MOASHA":
+            for m, c in zip(self.metric, cost):
+                experiment_result[m] = c
+            experiment_result[self.fidelity_type] = trial_info.budget
+            del experiment_result[self.metric]
+
+            self._solver.on_trial_add(trial=trial)
+
         self.trial_counter += 1
+
         self._solver.on_trial_complete(trial=trial, result=experiment_result)
         trial_result = trial.add_results(
             metrics=experiment_result,
@@ -228,6 +271,9 @@ class SynetuneOptimizer(Optimizer):
         """
         Return the best trial according to the provided metric
         """
+        if self.optimizer_name == "MOASHA":
+            self.solver.mode = "min"
+
         if self.solver.mode == "max":
             sign = 1.0
         else:
@@ -263,18 +309,69 @@ class SynetuneOptimizer(Optimizer):
 
         self.optimizer_kwargs.update(_optimizer_kwargs)
 
+        if self.optimizer_name == "MOASHA":
+            self.metric = self.optimizer_kwargs["metrics"]
+            del self.optimizer_kwargs["metric"]
+            del self.optimizer_kwargs["resource_attr"]
+
         bscheduler = optimizers_dict[self.optimizer_name](**self.optimizer_kwargs)
         return bscheduler
-    
-    def get_current_incumbent(self) \
-            -> Incumbent:
-        trial_result = self.best_trial(metric=self.metric)
-        trial_info = self.convert_to_trial(trial=trial_result)
-        cost = trial_result.metrics[self.metric]
-        trial_value = TrialValue(
-            cost=cost,
-            time=trial_result.seconds,
-            virtual_time=trial_result.seconds,
-            additional_info=trial_result.metrics
-        )
-        return (trial_info, trial_value)
+
+    def get_current_incumbent(self) -> Incumbent:
+        if isinstance(self.metric, str):
+            trial_result = self.best_trial(metric=self.metric)
+            trial_info = self.convert_to_trial(trial=trial_result)
+            cost = trial_result.metrics[self.metric]
+            trial_value = TrialValue(
+                cost=cost,
+                time=trial_result.seconds,
+                virtual_time=trial_result.seconds,
+                additional_info=trial_result.metrics,
+            )
+            return (trial_info, trial_value)
+
+        else:
+            # multiobjecti
+            max_budget = np.max(
+                [
+                    v.metrics[self.fidelity_type]
+                    for v in self.completed_experiments.values()
+                ]
+            )
+            highest_fidelity = [
+                v
+                for v in self.completed_experiments.values()
+                if v.metrics[self.fidelity_type] == max_budget
+            ]
+            hf_cost = np.array(
+                [[v.metrics[m] for m in self.metric] for v in highest_fidelity]
+            )
+
+            # # calculate the hypervolume on the highest fidelity!
+            # if not hasattr(self, 'ref_point'):
+            #     # calculate the reference point as relative margin of the highest fidelity points
+            #     ref_point = hf_cost.max(axis=0)
+            #     self.hv = self.HV(ref_point=ref_point)
+            # hv = self.hv(hf_cost)
+            non_dom = NonDominatedSorting().do(hf_cost, only_non_dominated_front=True)
+
+            trial_results = [
+                self.completed_experiments[key]
+                for index, key in enumerate(self.completed_experiments.keys())
+                if index in non_dom
+            ]
+
+            incumbents = [
+                (
+                    self.convert_to_trial(trial=trial_result),
+                    TrialValue(
+                        cost=[trial_result.metrics[m] for m in self.metric],
+                        time=trial_result.seconds,
+                        virtual_time=trial_result.seconds,
+                        additional_info=None,
+                    ),
+                )
+                for trial_result in trial_results
+            ]
+
+            return incumbents
