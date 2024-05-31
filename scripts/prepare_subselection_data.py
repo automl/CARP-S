0c8b60670ac24bcd4f87f515b02124e8657656bf
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2024-05-31 11:17:06 +0200

    refactor: prepare subselection data

--- scripts/prepare_subselection_data.py
+++ scripts/prepare_subselection_data.py
@@ -6,16 +6,21 @@ import pandas as pd
 import carps
 
 from carps.analysis.run_autorank import get_df_crit
-from carps.analysis.process_data import process_logs
+from carps.analysis.gather_data import normalize_logs, convert_mixed_types_to_str
+
+from carps.utils.loggingutils import get_logger, setup_logging
+
+setup_logging()
+logger = get_logger(__file__)
 
 
 if __name__=="__main__":
     paths = {
-    "MF": [
-        "runs/DEHB",
-        "runs/SMAC3-MultiFidelityFacade",
-        "runs//SMAC3-Hyperband",
-        ],
+        "MF": [
+            "runs/DEHB",
+            "runs/SMAC3-MultiFidelityFacade",
+            "runs/SMAC3-Hyperband",
+            ],
         "BB": [
             "runs/SMAC3-BlackBoxFacade",
             "runs/RandomSearch",
@@ -25,23 +30,26 @@ if __name__=="__main__":
     }
 
     normalize_peformance = True
-    extension = ".csv"
+
+    extension = ".parquet"
+    logger.info(f"Use extension {extension}")
 
     _log_fn = "logs" + extension
     _log_cfg_fn = "logs_cfg" + extension
     combined_fn = "logs_combined" + extension
     combined_cfg_fn = "logs_combined_cfg" + extension
     df_crit_fn = "pointfile.txt"
+    regather = True
 
     for scenario, _paths in paths.items():
-        print(scenario)
+        logger.info(scenario)
         identifier = scenario + "_"
-        if True:
+        if regather:
             dfs = []
             dfs_cfg = []
             for p in _paths:
                 p = Path(p)
-                print("Load", p.resolve())
+                logger.info(f"Load {str(p.resolve())}")
                 log_fn = p / _log_fn    
                 log_cfg_fn = p / _log_cfg_fn    
                 if not log_fn.is_file() or not log_cfg_fn.is_file():
@@ -59,12 +67,29 @@ if __name__=="__main__":
             df_cfg = pd.concat(dfs_cfg)
             del dfs
             del dfs_cfg
-            df.to_parquet(identifier + combined_fn)
-            df_cfg.to_parquet(identifier + combined_cfg_fn)
 
-        df = pd.read_parquet(identifier + combined_fn)
+            if extension == ".csv":
+                df.to_csv(identifier + combined_fn)
+                df_cfg.to_csv(identifier + combined_cfg_fn) 
+            else:
+                df.to_parquet(identifier + combined_fn)
+                df_cfg.to_parquet(identifier + combined_cfg_fn) 
+        else:
+            if extension == ".csv":
+                df = pd.read_csv(identifier + combined_fn)
+            else:
+                df = pd.read_parquet(identifier + combined_fn)
+        logger.info("Normalize logs...")
+        df = normalize_logs(df)
+        df = convert_mixed_types_to_str(df)
+        logger.info("Done")
         perf_col = "trial_value__cost_inc_norm" if normalize_peformance else "trial_value__cost_inc"
+        logger.info("Get final performance")
         df_crit = get_df_crit(df, perf_col=perf_col)
         df_crit.to_csv(identifier + df_crit_fn, sep=" ", index=False, header=False)
-        df_crit.to_parquet(identifier + "df_crit.parquet")
+        df_crit.to_csv(identifier + "df_crit.csv")
+        try:
+            df_crit.to_parquet(identifier + "df_crit.parquet")
+        except:
+            pass
 
