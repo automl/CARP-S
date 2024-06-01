68941995c85d8b73c7c78cb6d137a0ed6631154b
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2024-06-01 09:19:07 +0200

    fix: only convert when parquet missing

--- scripts/convert_logs_csv_to_parquet.py
+++ scripts/convert_logs_csv_to_parquet.py
@@ -11,18 +11,30 @@ from carps.analysis.gather_data import convert_mixed_types_to_str
 
 if __name__=="__main__":
     paths = {
-        "MF": [
-            "runs/DEHB",
-            "runs/SMAC3-Hyperband",
-            "runs/SMAC3-MultiFidelityFacade",
-            ],
-        "BB": [
-            "runs/SMAC3-BlackBoxFacade",
-            "runs/RandomSearch",
-            "runs/Nevergrad-CMA-ES",
+        # "MF": [
+        #     "runs/DEHB",
+        #     "runs/SMAC3-Hyperband",
+        #     "runs/SMAC3-MultiFidelityFacade",
+        #     ],
+        # "BB": [
+        #     "runs/SMAC3-BlackBoxFacade",
+        #     "runs/RandomSearch",
+        #     "runs/Nevergrad-CMA-ES",
+        # ],
+        "BBsubset": [
+            "runs_subset/SMAC3-BlackBoxFacade",
+            "runs_subset/SMAC3-1.4-BlackBoxFacade",
+            "runs_subset/SMAC3-HPOFacade",
+            "runs_subset/RandomSearch",
+            "runs_subset/Optuna",
+            "runs_subset/HEBO",
+            "runs_subset/Scikit_Optimize",
+            "runs_subset/SyneTune-BO",
+            "runs_subset/Nevergrad-CMA-ES",
         ],
 
     }
+    always_convert = False
 
     normalize_peformance = True
 
@@ -34,8 +46,8 @@ if __name__=="__main__":
     combined_cfg_fn = "logs_combined_cfg.parquet"
     df_crit_fn = "pointfile.txt"
 
-    def convert(fn_pq: Path, fn_csv: Path) -> None:
-        if fn_csv.is_file() and not fn_pq.is_file():
+    def convert(fn_pq: Path, fn_csv: Path, always_convert: bool = True) -> None:
+        if fn_csv.is_file() and not fn_pq.is_file() or always_convert:
             print("Convert", fn_csv, "to", fn_pq)
             df = pd.read_csv(fn_csv)
             df = convert_mixed_types_to_str(df)
@@ -48,8 +60,8 @@ if __name__=="__main__":
             log_fn_csv = Path(p) / _log_fn_csv    
             log_cfg_fn_csv = Path(p) / _log_cfg_fn_csv
 
-            convert(log_fn_pq, log_fn_csv)
-            convert(log_cfg_fn_pq, log_cfg_fn_csv)
+            convert(log_fn_pq, log_fn_csv, always_convert)
+            convert(log_cfg_fn_pq, log_cfg_fn_csv, always_convert)
 
             
 
