585f45a91a52740fffaa8faf4cdfb08c4cc38212
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2024-06-02 09:14:39 +0200

    fix: udpate to dev/test

--- scripts/run_BB_subselection.sh
+++ scripts/run_BB_subselection.sh
@@ -1,18 +1,23 @@
 OPT=$1
 echo $OPT
 
-# Subselection. 40
-python -m carps.run +cluster=noctua $OPT '+problem/subselection/blackbox=glob(*)' 'seed=range(1,21)' 'baserundir=runs_subset_BB' -m 
+# Subselection. 30
+python -m carps.run +cluster=noctua $OPT '+problem/subselection/blackbox/dev=glob(*)' 'seed=range(1,21)' 'baserundir=runs_subset_BB/dev' -m 
+python -m carps.run +cluster=noctua $OPT '+problem/subselection/blackbox/test=glob(*)' 'seed=range(1,21)' 'baserundir=runs_subset_BB/test' -m 
 
 
 
 
 # or per benchmark due to dependencies
 # HPOB
-# python -m carps.run +cluster=noctua $OPT '+problem/subselection/blackbox=glob(subset_hpob*)' 'seed=range(1,21)' 'baserundir=runs_subset' -m 
+# python -m carps.run +cluster=noctua $OPT '+problem/subselection/blackbox/dev=glob(subset_hpob*)' 'seed=range(1,21)' 'baserundir=runs_subset' -m 
+# python -m carps.run +cluster=noctua $OPT '+problem/subselection/blackbox/test=glob(subset_hpob*)' 'seed=range(1,21)' 'baserundir=runs_subset' -m 
 # # BBOB
-# python -m carps.run +cluster=noctua $OPT '+problem/subselection/blackbox=glob(subset_bbob*)' 'seed=range(1,21)' 'baserundir=runs_subset' -m 
+# python -m carps.run +cluster=noctua $OPT '+problem/subselection/blackbox/dev=glob(subset_bbob*)' 'seed=range(1,21)' 'baserundir=runs_subset' -m 
+# python -m carps.run +cluster=noctua $OPT '+problem/subselection/blackbox/test=glob(subset_bbob*)' 'seed=range(1,21)' 'baserundir=runs_subset' -m 
 # # HPOBench
-# python -m carps.run +cluster=noctua $OPT '+problem/subselection/blackbox=glob(subset_hpobench*)' 'seed=range(1,21)' 'baserundir=runs_subset' -m 
+# python -m carps.run +cluster=noctua $OPT '+problem/subselection/blackbox/dev=glob(subset_hpobench*)' 'seed=range(1,21)' 'baserundir=runs_subset' -m 
+# python -m carps.run +cluster=noctua $OPT '+problem/subselection/blackbox/test=glob(subset_hpobench*)' 'seed=range(1,21)' 'baserundir=runs_subset' -m 
 # # YAHPO
-# python -m carps.run +cluster=noctua $OPT '+problem/subselection/blackbox=glob(subset_yahpo*)' 'seed=range(1,21)' 'baserundir=runs_subset' -m 
+# python -m carps.run +cluster=noctua $OPT '+problem/subselection/blackbox/dev=glob(subset_yahpo*)' 'seed=range(1,21)' 'baserundir=runs_subset' -m 
+# python -m carps.run +cluster=noctua $OPT '+problem/subselection/blackbox/test=glob(subset_yahpo*)' 'seed=range(1,21)' 'baserundir=runs_subset' -m 
