fa7b49a0a4b7ecd5bfcbe76486eca49361fcea95
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2024-06-02 12:13:05 +0200

    fix: legend placement

--- carps/analysis/performance_over_time.py
+++ carps/analysis/performance_over_time.py
@@ -15,11 +15,11 @@ def plot_performance_over_time(df: pd.DataFrame, x="n_trials_norm", y="cost_inc_
     fig = plt.figure(figsize=figsize)
     ax = fig.add_subplot(111)
     ax = sns.lineplot(data=df, x=x, y=y, hue=hue, palette=palette, **lineplot_kwargs, ax=ax)
-    savefig(fig, figure_filename)
     if show_legend:
         ax.legend(loc='center left', bbox_to_anchor=(1.05, 0.5))
     else:
         ax.get_legend().remove()
+    savefig(fig, figure_filename)
     return fig, ax
 
 def plot_rank_over_time(df: pd.DataFrame, x="n_trials_norm", y="cost_inc_norm", hue="optimizer_id", figure_filename: str = "figures/performance_over_time.pdf", figsize: tuple[int,int]=(6,4), show_legend: bool = True, **lineplot_kwargs
