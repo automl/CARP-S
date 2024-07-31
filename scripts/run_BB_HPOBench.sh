43238ed906a39ef297bc00878be1480442e95b1a
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2024-07-31 19:43:21 +0200

    Add more HPOBench tasks

--- scripts/run_BB_HPOBench.sh
+++ scripts/run_BB_HPOBench.sh
@@ -4,6 +4,9 @@ echo $OPT
 # HPOBench surr. 7
 python -m carps.run +cluster=noctua $OPT '+problem/HPOBench/blackbox/surr=glob(*)' 'seed=range(1,21)' -m 
 
-# HPOBench tab. 88
-python -m carps.run +cluster=noctua $OPT '+problem/HPOBench/blackbox/tab=glob(*)' 'seed=range(1,11)' -m
-python -m carps.run +cluster=noctua $OPT '+problem/HPOBench/blackbox/tab=glob(*)' 'seed=range(11,21)' -m
+# HPOBench tab ML. 88
+python -m carps.run +cluster=noctua $OPT '+problem/HPOBench/blackbox/tab/ml=glob(*)' 'seed=range(1,11)' -m
+python -m carps.run +cluster=noctua $OPTs '+problem/HPOBench/blackbox/tab/ml=glob(*)' 'seed=range(11,21)' -m
+
+# HPOBench tab nas. 10
+python -m carps.run +cluster=noctua $OPT '+problem/HPOBench/blackbox/tab/nas=glob(*)' 'seed=range(1,21)' -m
