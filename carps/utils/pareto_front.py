170531d8a34bc3af8ba498a805c511ea886b8a08
benjamc <c.benjamins@ai.uni-hannover.de>
2025-01-15 09:38:52 +0100

    style(pareto_front)

--- carps/utils/pareto_front.py
+++ carps/utils/pareto_front.py
@@ -1,9 +1,24 @@
+"""Calculate the Pareto front of a set of points."""
+
 from __future__ import annotations
 
 import numpy as np
 
 
 def pareto(costs: np.ndarray) -> np.ndarray:
+    """Find the Pareto front of a set of points.
+
+    Parameters
+    ----------
+    costs : np.ndarray
+        The costs of the points. Each row is a point, and each column is a
+        dimension (N,D).
+
+    Returns:
+    --------
+    np.ndarray
+        A boolean array indicating which points are on the Pareto front (N,).
+    """
     is_pareto = np.ones(costs.shape[0], dtype=bool)
     for i, c in enumerate(costs):
         if is_pareto[i]:
