73546a20207dc603f14c346c8e5245f7e9c3815a
benjamc <c.benjamins@ai.uni-hannover.de>
2025-03-04 17:20:24 +0100

    refactor(install): update

--- container_recipes/benchmarks/HPOBench/install_HPOBench.sh
+++ container_recipes/benchmarks/HPOBench/install_HPOBench.sh
@@ -14,5 +14,6 @@ else
     CONDA_RUN_COMMAND="${CONDA_COMMAND} run ${CONDA_ENV_NAME}"
 fi
 $CONDA_RUN_COMMAND $PIP install git+https://github.com/automl/HPOBench.git@fix/numpy_deprecation
+$CONDA_RUN_COMMAND $PIP install openml
 $CONDA_RUN_COMMAND $PIP install ConfigSpace --upgrade
 $CONDA_RUN_COMMAND python container_recipes/benchmarks/HPOBench/prepare_nas_benchmarks.py
