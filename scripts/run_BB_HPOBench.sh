dc20c73817678c103d99df95a45a020064da5821
benjamc <c.benjamins@ai.uni-hannover.de>
2025-03-18 18:00:50 +0100

    refactor(hpobench_configs): tab -> tabular

--- scripts/run_BB_HPOBench.sh
+++ scripts/run_BB_HPOBench.sh
@@ -5,8 +5,8 @@ echo $OPT
 python -m carps.run +cluster=noctua $OPT '+task/HPOBench/blackbox/surr=glob(*)' 'seed=range(1,21)' -m 
 
 # HPOBench tab ML. 88
-python -m carps.run +cluster=noctua $OPT '+task/HPOBench/blackbox/tab/ml=glob(*)' 'seed=range(1,11)' -m
-python -m carps.run +cluster=noctua $OPTs '+task/HPOBench/blackbox/tab/ml=glob(*)' 'seed=range(11,21)' -m
+python -m carps.run +cluster=noctua $OPT '+task/HPOBench/blackbox/tabular/ml=glob(*)' 'seed=range(1,11)' -m
+python -m carps.run +cluster=noctua $OPTs '+task/HPOBench/blackbox/tabular/ml=glob(*)' 'seed=range(11,21)' -m
 
 # HPOBench tab nas. 10
-python -m carps.run +cluster=noctua $OPT '+task/HPOBench/blackbox/tab/nas=glob(*)' 'seed=range(1,21)' -m
+python -m carps.run +cluster=noctua $OPT '+task/HPOBench/blackbox/tabular/nas=glob(*)' 'seed=range(1,21)' -m
