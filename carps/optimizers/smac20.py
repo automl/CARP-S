b7293c608fc833f84adfe6425bfd97c2363a2f33
benjamc <c.benjamins@ai.uni-hannover.de>
2025-12-13 16:18:16 +0100

    feat(adaptive_capping): pass cutoff around

--- carps/optimizers/smac20.py
+++ carps/optimizers/smac20.py
@@ -122,7 +122,12 @@ class SMAC3Optimizer(Optimizer):
         return configspace
 
     def convert_to_trial(  # type: ignore[override]
-        self, config: Configuration, seed: int | None = None, budget: float | None = None, instance: str | None = None
+        self,
+        config: Configuration,
+        seed: int | None = None,
+        budget: float | None = None,
+        instance: str | None = None,
+        cutoff: float | None = None,
     ) -> TrialInfo:
         """Convert proposal from SMAC to TrialInfo.
 
@@ -143,10 +148,15 @@ class SMAC3Optimizer(Optimizer):
             Trial info containing configuration, budget, seed, instance.
         """
         inst = int(instance) if instance is not None else None
-        return TrialInfo(config=config, seed=seed, budget=budget, instance=inst)
+        return TrialInfo(config=config, seed=seed, budget=budget, instance=inst, cutoff=cutoff)
 
     def target_function(
-        self, config: Configuration, seed: int | None = None, budget: float | None = None, instance: str | None = None
+        self,
+        config: Configuration,
+        seed: int | None = None,
+        budget: float | None = None,
+        instance: str | None = None,
+        cutoff: float | None = None,
     ) -> float | list[float]:
         """Target Function.
 
@@ -162,13 +172,15 @@ class SMAC3Optimizer(Optimizer):
             Budget, by default None
         instance : str | None, optional
             Instance, by default None
+        cutoff : float | None, optional
+            Optional cutoff argument for adaptive capping.
 
         Returns:
         -------
         float | list[float]
             Cost as float or list[float], depending on the number of objectives.
         """
-        trial_info = self.convert_to_trial(config=config, seed=seed, budget=budget, instance=instance)
+        trial_info = self.convert_to_trial(config=config, seed=seed, budget=budget, instance=instance, cutoff=cutoff)
         trial_value = self.task.objective_function.evaluate(trial_info=trial_info)
         return trial_value.cost
 
